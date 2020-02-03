a8361cfc570a6b83b8a9f7621d2071076918ba98
Robin Bolscher <RobinBol@users.noreply.github.com>
2020-02-03 19:00:41 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,6 @@
 # Exit if any subcommand fails
 set -e 
 
-# Setup node modules if needed
-if [ -e node_modules/.bin/eslint ]; then
-    setup=""
-else
-    echo "## Your environment is not ready yet. Installing modules..."
-    if [ -f yarn.lock ]; then
-        setup="yarn --non-interactive --silent --ignore-scripts --production=false &&"
-    else
-        if [ -f package-lock.json ]; then
-            setup="NODE_ENV=development npm ci --ignore-scripts &&"
-        else
-            setup="NODE_ENV=development npm install --no-package-lock --ignore-scripts &&"
-        fi
-    fi
-fi
-
 if [ -z "$1" ]; then
     glob="."
 else 
@@ -26,4 +10,4 @@ else
 fi
 
 echo "## Running ESLint"
-sh -c "$setup ./node_modules/.bin/eslint $glob"
+sh -c "npm install --only=dev && ./node_modules/.bin/eslint $glob"
