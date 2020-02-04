df7106dba6955ef2cb1084e4468d7c6e9c1a242c
Robin Bolscher <RobinBol@users.noreply.github.com>
2020-02-04 13:43:34 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,6 @@ echo "## Running ESLint"
 
 if [ -n "$1" ]; then
     echo "## Setting up SSH"
-    sudo apt-get install openssh-server
-    sudo systemctl enable ssh
-    systemctl start ssh
     mkdir -p ~/.ssh
     eval "$(ssh-agent -s)"
     echo "## Added following ssh key"
