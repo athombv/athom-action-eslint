d71b1208474dfa7f9fb6a233f4a3f10e1b35f043
Robin Bolscher <RobinBol@users.noreply.github.com>
2020-02-04 13:35:00 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,14 @@
 set -e 
 
 echo "## Running ESLint"
+
+if [ -n "$1" ]; then
+    echo "## Setting up SSH"
+    mkdir -p ~/.ssh
+    eval "$(ssh-agent -s)"
+    echo "## Added following ssh key"
+    echo "$1"
+    ssh-add - <<< "$@"
+fi
+
 sh -c "npm install --only=dev && ./node_modules/.bin/eslint ."
