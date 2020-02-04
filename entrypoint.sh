0320d2efd5c053c26c7ad4fb7c0e7c9ac5be06a0
Robin Bolscher <RobinBol@users.noreply.github.com>
2020-02-04 13:19:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,5 @@
 # Exit if any subcommand fails
 set -e 
 
-if [ -z "$1" ]; then
-    echo "## Setting up SSH"
-    mkdir -p ~/.ssh
-    eval "$(ssh-agent -s)"
-    echo "## Added following ssh key"
-    echo "$@"
-    ssh-add - <<< "$@"
-fi
-
 echo "## Running ESLint"
 sh -c "npm install --only=dev && ./node_modules/.bin/eslint ."
