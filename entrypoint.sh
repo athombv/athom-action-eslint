3241a94f34fccb5bba4023a873d57d41443804d8
Robin Bolscher <RobinBol@users.noreply.github.com>
2020-02-05 09:14:15 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,12 @@ if [ -n SSH_KEY ]; then
     ssh-add - <<<"${SSH_KEY}"
     ssh-keyscan -H github.com >> ~/.ssh/known_hosts
     ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts
+    
+    mkdir -m 700 $HOME/.ssh
+    touch -m 600 $HOME/.ssh/known_hosts
+    ssh-keyscan -H github.com > $HOME/.ssh/known_hosts
+    ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts
+    
     ls -a | grep .ssh
     echo "## Added SSH key"
 fi
